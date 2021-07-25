795749702fac746b67340e441a3a7e157e24f312
Stefan Costin <stefanfcostin@gmail.com>
2021-07-25 09:05:21 +0300

    Add authorization on the node routes

--- src/Unison.Cloud.Web/Controllers/NodesController.cs
+++ src/Unison.Cloud.Web/Controllers/NodesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,7 @@ using Unison.Cloud.Web.Utilities;
 
 namespace Unison.Cloud.Web.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class NodesController : ControllerBase
